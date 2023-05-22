e963d1f8e8a4f95dc5a4ab9e59a63a4f7ddb98f6
esh0rka <brainett13@gmail.com>
2023-05-23 02:47:45 +0300

    Bug fix, add sysarg, delete serverUpload.py

--- get_rates.py
+++ get_rates.py
@@ -1,14 +1,18 @@
 from pyoxr import OXRClient
 import sqlite3
+import boto3
+import sys
+
 
 conn = sqlite3.connect('current_exchange_rate.db')
 cursor = conn.cursor()
+cursor.execute('DROP TABLE current_exchange_rate')
 cursor.execute('''CREATE TABLE IF NOT EXISTS current_exchange_rate
                   (id INTEGER PRIMARY KEY AUTOINCREMENT,
                    currency TEXT NOT NULL,
                    rate INTEGER)''')
 
-oxr_cli = OXRClient(app_id='6acee60892d849bfaa7d445181a112c1')
+oxr_cli = OXRClient(app_id=sys.argv[1])
 result = oxr_cli.get_latest()
 
 for currency, rate in result['rates'].items():
@@ -16,3 +20,11 @@ for currency, rate in result['rates'].items():
 
 conn.commit()
 conn.close()
+
+session = boto3.session.Session()
+s3 = session.client(
+    service_name='s3',
+    endpoint_url='https://storage.yandexcloud.net'
+)
+
+s3.upload_file('current_exchange_rate.db', 'change-rates', 'current_exchange_rate.db')
